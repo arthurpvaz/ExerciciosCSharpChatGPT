0f1890651dafe1277d18fda172feb001c2421cf7
Arthur Vaz <pv.arthur10@gmail.com>
2024-06-27 17:51:04 -0300

    Criação da classe person

---
+++ Ex1/Ex1/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; private set; }
+        public DateTime Birth {  get; set; }
+
+        public Person (string name, DateTime birth)
+        {
+            Name = name;
+            Birth = birth;
+            Age = CalculteAge();
+        }
+
+        private int CalculteAge()
+        {
+            return (DateTime.Now.Subtract(Birth).Days) / 365; // Subtract the actually date with the birth day and return the age
+        }
+    }
+}
---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Arthur", new DateTime(2004, 10, 20));
+
+            Console.WriteLine("Age: " + p1.Age);
+        }
+    }
+}
